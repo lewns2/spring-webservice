dd82cb19a327d062e23a4fd54aadb1db8af6da84
lewns2 <lewns2002@naver.com>
2021-03-12 15:18:34 +0900

    Posts 등록 API 및 테스트 코드 추가

---
+++ spring/src/main/java/com/salon/springboot/service/posts/PostsService.java
@@ -0,0 +1,22 @@
+package com.salon.springboot.service.posts;
+
+import com.salon.springboot.domain.posts.PostsRepository;
+import com.salon.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+
+
+}
